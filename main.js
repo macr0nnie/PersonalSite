88d133d9ca5278a1333b416ce6bd2049befbec95
macr0nnie <macr0nnieasmr@gmail.com>
2023-09-29 21:52:39 -0400

    Co-authored-by: macr0nnie <macr0nnieasmr@gmail.com>

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // This is a javascript comment in a javascript source file
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    document.getElementById('time').textContent = timeString;
+}
 
+// Update the clock every second
+setInterval(updateTime, 1000);
+
+// Initial call to display the time
+updateTime();
